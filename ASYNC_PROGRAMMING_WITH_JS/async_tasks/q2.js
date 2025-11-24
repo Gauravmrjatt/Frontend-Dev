5ee02e5975fdada19d6be4b2ebca168e44c691a9
Yashkhandelwal683 <khandelwalyash355@gmail.com>
2025-11-24 10:25:49 +0530

    asignment done

--- ASYNC_PROGRAMMING_WITH_JS/async_tasks/q2.js
+++
@@ -1,6 +0,0 @@
-// Q2 – Micro vs Macro Task Order
-console.log('Start');
-setTimeout(() => console.log('Macrotask: setTimeout'), 0);
-Promise.resolve().then(() => console.log('Microtask: Promise.then'));
-console.log('Synchronous Log');
-console.log('End');
